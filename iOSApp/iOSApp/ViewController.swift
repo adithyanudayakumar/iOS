7175ccf08acc438f4b1de2527473ffd4f70c42ac
adithyanudayakumar <adithyan.udayakumar@gmail.com>
2017-07-02 17:30:03 -0400

    Second Commit

---
+++ iOSApp/iOSApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOSApp
+//
+//  Created by adithyan udayakumar on 7/2/17.
+//  Copyright © 2017 adithyan udayakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
